f1f8147b5ffc0ed8779a66e093828b39963fca16
danieldemelash <danieldemelash5@gmail.com>
2025-01-17 20:18:40 +0300

    style: add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'ቅዳሜ ገቢያ',
       debugShowCheckedModeBanner: false,
       home: Scaffold(),
     );
