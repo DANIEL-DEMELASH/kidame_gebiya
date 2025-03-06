a7e505bcbc81c5184ec0000e16586b9a8955714b
danieldemelash <danieldemelash5@gmail.com>
2025-03-06 16:01:23 +0300

    update: dio connect & receive timeout

--- lib/core/dio/dio_config.dart
+++ lib/core/dio/dio_config.dart
@@ -1,7 +1,7 @@
 class DioConfig {
   static const String baseUrl = 'https://fakestoreapi.com/';
-  static const int connectTimeout = 5; // 5 seconds
-  static const int receiveTimeout = 3; // 3 seconds
+  static const int connectTimeout = 7; // 7 seconds
+  static const int receiveTimeout = 5; // 5 seconds
   static const int sendTimeout = 5; // 5 seconds
   
   static const String login = 'auth/login';
