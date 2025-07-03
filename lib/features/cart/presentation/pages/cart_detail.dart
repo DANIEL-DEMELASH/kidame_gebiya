2c4e83906065a476838fc4387f0b8bb8f36fe6a3
DANIEL-DEMELASH <danieldemelash5@gmail.com>
2025-07-03 10:09:32 +0300

    refactor: cart product widget

--- lib/features/cart/presentation/pages/cart_detail.dart
+++ lib/features/cart/presentation/pages/cart_detail.dart
@@ -1,14 +1,15 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-// import 'package:intl/intl.dart';
+
 import 'package:kidame_gebiya/app_constant.dart';
 import 'package:kidame_gebiya/features/cart/bloc/product_by_id/product_by_id_bloc.dart';
 import 'package:kidame_gebiya/features/cart/bloc/product_by_id/product_by_id_event.dart';
 import 'package:kidame_gebiya/features/cart/bloc/product_by_id/product_by_id_state.dart';
 import 'package:kidame_gebiya/features/cart/data/models/cart_model.dart';
+
+import '../widgets/cart_product_card.dart';
 
 class CartDetail extends StatelessWidget {
   const CartDetail({super.key, required this.cartModel});
@@ -49,77 +50,9 @@ class CartDetail extends StatelessWidget {
                   }
                   
                   if(state is ProductByIdLoaded){
-                    return ListTile(
-                      contentPadding: const EdgeInsets.only(top: 4, bottom: 4, right: 8),
-                      leading: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Checkbox(
-                            value: false, 
-                            onChanged: (value){
-                          }),
-                          
-                          CachedNetworkImage(
-                            imageUrl: state.product.image, fit: BoxFit.contain,
-                            placeholder: (context, url) => const CircularProgressIndicator(),
-                            errorWidget: (context, url, error) => const Placeholder(),
-                          )
-                        ],
-                      ),
-                      title: Padding(
-                        padding: const EdgeInsets.only(bottom: 8),
-                        child: Text(state.product.title, maxLines: 1, overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 12, color: darkGreyColor),),
-                      ),
-                      subtitle: Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("\$${state.product.price}", 
-                              style: const TextStyle(color: Colors.black, fontWeight: FontWeight.w500),),
-                              
-                            Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                GestureDetector(
-                                  onTap: () {
-                                    
-                                  },
-                                  child: Container(
-                                    width: 20,
-                                    height: 20,
-                                    decoration: BoxDecoration(
-                                      border: Border.all(color: primaryColor)
-                                    ),
-                                    child: const Text('-', textAlign: TextAlign.center, style: TextStyle(color: primaryColor, fontWeight: FontWeight.w500),),
-                                  ),
-                                ),
-                                
-                                const SizedBox(width: 12),
-                                
-                                Text(product.quantity.toString()),
-                                
-                                const SizedBox(width: 12),
-                                
-                                GestureDetector(
-                                  onTap: () {
-                                    
-                                  },
-                                  child: Container(
-                                    width: 20,
-                                    height: 20,
-                                    decoration: BoxDecoration(
-                                      border: Border.all(color: primaryColor)
-                                    ),
-                                    child: const Text('+', textAlign: TextAlign.center, style: TextStyle(color: primaryColor, fontWeight: FontWeight.w500),),
-                                  ),
-                                )
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
+                    return CartProductCard(
+                      product: state.product, 
+                      cartProduct: product
                     );
                   }
                   
